84d16297c9048799b22140145ebd6648ee69fcf2
MagesTicRoboT <abdu;rauf735@gmail.com>
2023-06-15 22:45:21 +0500

    change in rule

--- app/Http/Controllers/BarCodeController.php
+++ app/Http/Controllers/BarCodeController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers;
 
 use Milon\Barcode\DNS1D;
-use Milon\Barcode\DNS2D;
 use Illuminate\Http\Request;
+use App\Rules\MaxGreaterThanMin;
 use Illuminate\Support\Facades\Storage;
+use Barryvdh\Snappy\Facades\SnappyImage;
 
 class BarCodeController extends Controller
 {
@@ -13,23 +14,30 @@ class BarCodeController extends Controller
     {
         $request->validate([
             'mask' => ['required', 'string'],
-            'min' => ['required', 'integer'],
-            'max' => ['required', 'integer'],
+            'min' => ['required', 'integer', 'min:1'],
+            'max' => ['required', 'integer', new MaxGreaterThanMin()],
         ]);
 
         $mask = $request->mask;
         $min = $request->min;
         $max = $request->max;
 
-        for($i = $min; $i<=$max; $i++){
-            $barcode = $mask."".$i;
-            $image_name = $barcode.'.png';
-            $file = base64_decode(DNS1D::getBarcodePNG($barcode, 'C39',1,60,array(1,1,1), true));
-            Storage::disk('public')->put('barcode/'.$image_name, $file);
 
-        }
+        // for($i = $min; $i<=$max; $i++){
+        //     $barcode = $mask."".$i;
+        //     $image_name = $barcode.'.png';
+        //     $file = base64_decode(DNS1D::getBarcodePNG($barcode, 'C39',1,60,array(1,1,1), true));
+        //     Storage::disk('public')->put('barcode/'.$image_name, $file);
+
+        // }
+        $companyName = 'Your Company';
+
+        $html = view('barcode', compact('companyName', 'mask'))->render();
+        $imagePath = storage_path('app/public/barcode/' . $mask . '.png');
+
+        SnappyImage::loadHTML($html)->save($imagePath);
+
         
-
         return back();
     }
 }
